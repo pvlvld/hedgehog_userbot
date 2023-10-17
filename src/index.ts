58eb5427acc15eabab9d05726dfae6b613b6ef65
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-10-18 01:16:19 +0300

    remove unusable cmd name var

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import createCommands, { ICommands } from './helpers/createCommands';
 const commands = createCommands();
 
 async function startFarming(commands: ICommands) {
-  for (const [name, command] of Object.entries(commands)) {
+  for (const [, command] of Object.entries(commands)) {
     command.call_repeated();
     await sleep(3333); // 3s sleep
   }
