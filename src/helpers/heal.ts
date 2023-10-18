ebb0d84b1a958db08b48825c3f94fd33cc7eb7e7
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-10-18 18:27:37 +0300

    use getHouseMesssage

--- src/helpers/heal.ts
+++ src/helpers/heal.ts
@@ -1,17 +1,8 @@
-import client from './client';
-import getBotEntity from './getBotEntity';
+import getHouseMessage from './getHouseMessage';
 
 async function heal() {
-  const healMessage = (
-    await (
-      await client
-    ).getMessages(await getBotEntity(), {
-      ids: 90962,
-    })
-  )[0];
-  if (healMessage.buttons) {
-    await healMessage.click(healMessage.buttons[0][1]);
-  }
+  const healMessage = await getHouseMessage();
+  if (healMessage.buttons) await healMessage.click(healMessage.buttons[0][1]);
 }
 
 export default heal;
