e644f96c70bc5cd3b170688c977b6191cef2dfe5
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-10-18 20:34:06 +0300

    init CreateHedgehogsArmy

---
+++ src/helpers/CreateHedgehogsArmy.ts
@@ -0,0 +1,31 @@
+import getClients from './getClients';
+import createCommands, { type ICommands } from './createCommands';
+import { sleep } from 'telegram/Helpers';
+import startFarming from './startFarming';
+
+type IHedgehogsArmy = {
+  hedgehogs: ICommands[];
+  startFarming: () => void;
+};
+
+async function CreateHedgehogsArmy() {
+  const clients = await getClients();
+
+  const hedgehogsArmy: IHedgehogsArmy = {
+    hedgehogs: [] as ICommands[],
+  } as any;
+
+  for (const client of clients) {
+    hedgehogsArmy.hedgehogs.push(await createCommands(client));
+  }
+
+  hedgehogsArmy.startFarming = () => {
+    for (const hedgehog of hedgehogsArmy.hedgehogs) {
+      startFarming(hedgehog);
+    }
+  };
+
+  return hedgehogsArmy;
+}
+
+export default CreateHedgehogsArmy;
