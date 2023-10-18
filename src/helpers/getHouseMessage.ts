60b2c8021c86708015326b17aabf30403c3f0693
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-10-18 20:32:34 +0300

    use client

--- src/helpers/getHouseMessage.ts
+++ src/helpers/getHouseMessage.ts
@@ -1,17 +1,19 @@
-import { sleep } from 'telegram/Helpers';
+import { type MyClient } from './getClients';
 import sendMessage from './sendMessage';
+import { sleep } from 'telegram/Helpers';
 import getLastMessage from './getLastMessage';
 
-async function getHouseMessage(attempt = 1) {
+async function getHouseMessage(client: MyClient, attempt = 1) {
   console.info(`Getting house message. Attempt: ${attempt}...`);
 
-  await sendMessage('/house');
+  await sendMessage(client, '/house');
 
   await sleep(1000);
 
-  const house = await getLastMessage();
+  const house = await getLastMessage(client);
 
-  if (house.text !== '🇺🇦 Оберіть дію 👀') return getHouseMessage(attempt++);
+  if (house.text !== '🇺🇦 Оберіть дію 👀')
+    return getHouseMessage(client, attempt++);
 
   console.info('House message successfully received!');
 
