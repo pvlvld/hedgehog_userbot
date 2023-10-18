a7405a3b6a742459c10a4436bb021ab67dadb4b0
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-10-18 20:30:54 +0300

    use client

--- src/helpers/getBotEntity.ts
+++ src/helpers/getBotEntity.ts
@@ -1,14 +1,14 @@
-import client from './client';
+import { MyClient } from './getClients';
 
 const TARGET_BOT_ID = process.env.TARGET_BOT_ID;
 
-async function getBotEntity() {
+async function getBotEntity(client: MyClient) {
   if (!Number(TARGET_BOT_ID))
     throw new Error('Bad TARGET_BOT_ID. Check .env file.');
 
-  await (await client).getDialogs();
+  await client.getDialogs();
 
-  const botEntity = (await client).getInputEntity(Number(TARGET_BOT_ID));
+  const botEntity = client.getInputEntity(Number(TARGET_BOT_ID));
 
   return botEntity;
 }
