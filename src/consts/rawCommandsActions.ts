043d538eaca214177c241ca3de5a2b358ccde15a
Vlad Pavlenko <pavlenkoliar@gmail.com>
2023-10-28 01:01:33 +0300

    update training cooldown

--- src/consts/rawCommandsActions.ts
+++ src/consts/rawCommandsActions.ts
@@ -4,7 +4,7 @@ type RAW_COMMANDS_ACTIONS = (typeof RAW_COMMANDS_ACTIONS)[number][0];
 const RAW_COMMANDS_ACTIONS = [
   ['adventures', 167],
   ['read', 300],
-  ['training', 100],
+  ['training', 120],
   ['update_level', 1440],
   ['garden', 1440],
 ] as const;
